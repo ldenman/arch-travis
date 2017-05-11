46229b970f1c5bbba7d51155b3cadb7362ca5f55
Lake Denman <lake@lakedenman.com>
2017-05-11 12:57:52 -0700

    Changes

--- run-ci.sh
+++ run-ci.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-HOME=`mktemp -d` emacs --batch -nw --eval "(load-file \"common.el\")(load-file \"$1\"))"
+HOME=`mktemp -d` emacs --batch --eval "(load-file \"common.el\")(load-file \"$1\")(save-buffers-kill-terminal))"
